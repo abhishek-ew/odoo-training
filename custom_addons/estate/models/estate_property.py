2c91bb04d2626703764033dd23edb2983c05210a
abhishek-ew <abhishekf@ewisl.net>
2025-05-21 22:06:47 +0530

    feat(chapter-06): learned list, forms & search views

--- custom_addons/estate/models/estate_property.py
+++ custom_addons/estate/models/estate_property.py
@@ -1,16 +1,22 @@
 from odoo import models, fields
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
 class EstatePropertyModel(models.Model):
     _name = "estate.property"
     _description = "Estate Property Model"
 
+    def _default_date_availability(self):
+        #in 3 months from today
+        return date.today() + relativedelta(months=3)
+  
     name = fields.Char(string="Property Name", required=True)
     description = fields.Text(string="Description")
     postcode = fields.Char(string="Postcode") 
-    date_availability = fields.Date(string="Availability Date")
+    date_availability = fields.Date(string="Availability Date", copy=False, default=_default_date_availability)
     expected_price = fields.Float(string="Expected Price", required=True)
-    selling_price = fields.Float(string="Selling Price")
-    bedrooms = fields.Integer(string="Bedrooms")
+    selling_price = fields.Float(string="Selling Price", readonly=True, copy=False)
+    bedrooms = fields.Integer(string="Bedrooms", default=2)
     living_area = fields.Integer(string="Living Area")
     facades = fields.Integer(string="Facades")
     garage = fields.Boolean(string="Garage")
@@ -30,8 +36,8 @@ class EstatePropertyModel(models.Model):
     state = fields.Selection(
         [
             ("new", "New"),
-            ("offer received", "Offer Received"),
-            ("offer accepted", "Offer Accepted"),
+            ("received", "Offer Received"),
+            ("accepted", "Offer Accepted"),
             ("sold", "Sold"),
             ("cancelled", "Cancelled"),
         ],
